63ebc475181da8630c4304de5bf09c98bca9d334
Tiotedd <andrelsbrito2022@gmail.com>
2023-01-09 11:27:35 -0300

    changed the path of img Avatar-light.png

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function toggleMode() {
 
   if(html.classList.contains('light')){
     img.setAttribute(
-      'src', './assets/avatar-light.png'
+      'src', './assets/Avatar-light.png'
     );
     img.setAttribute(
       "alt",
